aea88381c160725aae9702a31e47db0e968ae18c
Anderson Marques <anderson.marques@acception.com.br>
2015-05-20 18:39:20 -0300

    commit

--- khipu/validacao/validacao_controle.py
+++ khipu/validacao/validacao_controle.py
@@ -1,9 +1,15 @@
 __author__ = 'anderson'
-from oauthlib.oauth2 import RequestValidator, BearerToken
+from oauthlib.oauth2 import RequestValidator, BearerToken, WebApplicationServer
 
 
 class Validador(RequestValidator):
+    def validate_client(self):
+        pass
+
     def validate_client_id(self, client_id, request, *args, **kwargs):
+        pass
+
+    def validate_client_secret(self):
         pass
 
 
--- khipu/views/view_autorizador.py
+++ khipu/views/view_autorizador.py
@@ -1,5 +1,6 @@
 __author__ = 'anderson'
 from pyramid.view import view_config, forbidden_view_config
+from khipu.validacao.validacao_controle import Validador
 
 
 class GeradoraAccessToken():
@@ -8,4 +9,7 @@ class GeradoraAccessToken():
 
     @view_config(route_name="token", renderer="json")
     def token(self):
+        json_credentials = self.request.json_body
+        req_validador = Validador()
+        print(json_credentials)
         return {"Anderson": "Lanna"}
